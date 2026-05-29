a2f8588bfd9a0c4d53b67a5c484844d5e16f0e5c
Vilém Zouhar <vilem.zouhar@gmail.com>
2026-05-29 09:49:50 +0200

    typo

--- scripts/01-quest.sh
+++ scripts/01-quest.sh
@@ -3,5 +3,6 @@ rsync -azP --filter=":- .gitignore" --exclude .git/ . ltb:/home/zouhar/last-tran
 
 # pull db from remote
 rsync -azP ltb:/home/zouhar/last-translation-benchmark/data/db.sqlite data/
+rsync -azP ltb:/home/zouhar/submissions.json data/
 
 python3 server --host-public "https://last-translation-benchmark.vilda.net" --host "0.0.0.0" --port 80
